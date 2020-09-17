9af02db410749f9d460e52574a47c2b26a68a2c0
neyensian <neyensian@urbandaleschools.com>
2020-09-17 10:26:06 -0500

    a

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,21 +1,21 @@
 '''
-Sign your name:________________
+Sign your name:Ian Neyens
 
 1.) How do you enter a single line comment in a program? Give an example.
-
+ # Single Line Comment
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
 What are the outputs? If the output is an error record the error and try to determine what the error is!
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = error, B is undefined
+type(42)            = class int
+type(42.0)          = class float
+type("C3PO")        = class str
+type(True)          = class bool
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
@@ -26,8 +26,8 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15.0
+type(a)       = class float
 
 
 4.) What is the mistake in the following code. Fix it!
@@ -36,6 +36,7 @@ x,y = (4,5)
 a = 3(x + y)
 a
 
+a = 3*(x + y)
 
 5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
@@ -43,5 +44,6 @@ x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
 
+ave = (x+y+z)/3
 
 '''
